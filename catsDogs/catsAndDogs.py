dd096728379c660d5a44eaeef7287799e5474510
lbrock98 <85496132+lbrock98@users.noreply.github.com>
2022-01-30 17:52:13 -0500

    Add files via upload

---
+++ catsDogs/catsAndDogs.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 30 17:37:12 2022
+
+@author: Lucy
+"""
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms, models # add models to the list
+from torchvision.utils import make_grid
+import os
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from helper import ConvolutionalNetwork
+import time
+
+# image transormations
+train_transform = transforms.Compose([
+        transforms.RandomRotation(10),      # rotate +/- 10 degrees
+        transforms.RandomHorizontalFlip(),  # reverse 50% of images
+        transforms.Resize(224),             # resize shortest side to 224 pixels
+        transforms.CenterCrop(224),         # crop longest side to 224 pixels at center
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                             [0.229, 0.224, 0.225])
+    ])
+
+test_transform = transforms.Compose([
+        transforms.Resize(224),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                             [0.229, 0.224, 0.225])
+    ])
+
+# load in data
+root = 'CATS_DOGS'
+train_data = datasets.ImageFolder(os.path.join(root, 'train'), transform=train_transform)
+test_data = datasets.ImageFolder(os.path.join(root, 'test'), transform=test_transform)
+
+torch.manual_seed(42)
+train_loader = DataLoader(train_data, batch_size=10, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=10, shuffle=True)
+
+class_names = train_data.classes
+
+# create model
+torch.manual_seed(101)
+CNNmodel = ConvolutionalNetwork()
+criterion = nn.CrossEntropyLoss() #for classification
+optimizer = torch.optim.Adam(CNNmodel.parameters(), lr=0.001)
+
+# train model
+start_time = time.time()
+
+epochs = 3 #gets us to about 70% accuracy
+
+max_trn_batch = 800 
+max_tst_batch = 300 
+
+train_losses = []
+test_losses = []
+train_correct = []
+test_correct = []
+
+for i in range(epochs):
+    trn_corr = 0
+    tst_corr = 0
+    
+    # Run the training batches
+    for b, (X_train, y_train) in enumerate(train_loader):
+        
+        # Limit the number of batches during training
+        if b == max_trn_batch:
+            break
+        b+=1
+        
+        # Apply the model
+        y_pred = CNNmodel(X_train)
+        loss = criterion(y_pred, y_train)
+ 
+        # Tally the number of correct predictions
+        predicted = torch.max(y_pred.data, 1)[1]
+        batch_corr = (predicted == y_train).sum()
+        trn_corr += batch_corr
+        
+        # Update parameters
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # Print interim results
+        if b%200 == 0:
+            print(f'epoch: {i:2}  batch: {b:4} [{10*b:6}/8000]  loss: {loss.item():10.8f}  \
+accuracy: {trn_corr.item()*100/(10*b):7.3f}%')
+
+    train_losses.append(loss)
+    train_correct.append(trn_corr)
+
+    # Run the testing batches
+    with torch.no_grad():
+        for b, (X_test, y_test) in enumerate(test_loader):
+            # Limit the number of batches
+            if b == max_tst_batch:
+                break
+
+            # Apply the model
+            y_val = CNNmodel(X_test)
+
+            # Tally the number of correct predictions
+            predicted = torch.max(y_val.data, 1)[1] 
+            tst_corr += (predicted == y_test).sum()
+
+    loss = criterion(y_val, y_test)
+    test_losses.append(loss)
+    test_correct.append(tst_corr)
+
+print(f'\nDuration: {(time.time() - start_time)/60:.0f} minutes') 
+
+torch.save(CNNmodel.state_dict(), 'CustomImageCNNModel.pt')
+
+# graph results
+plt.plot(train_losses, label='training loss')
+plt.plot(test_losses, label='validation loss')
+plt.title('Loss at the end of each epoch')
+plt.legend()
+
+plt.plot([t/80 for t in train_correct], label='training accuracy')
+plt.plot([t/30 for t in test_correct], label='validation accuracy')
+plt.title('Accuracy at the end of each epoch')
+plt.legend()
+
+
